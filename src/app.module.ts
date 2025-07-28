d08520266280eb73fd52c8994420c40b3c7915e9
spiercer <snowpiercer2001@gmail.com>
2025-07-28 12:31:19 +0300

    inject http module for axios in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CalendersModule } from './calenders/calenders.module';
 import { ConfigModule } from '@nestjs/config';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [CalendersModule, ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), HttpModule],
   controllers: [AppController],
   providers: [AppService],
 })
