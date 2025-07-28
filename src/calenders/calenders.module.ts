ddfdb6599336c771787b81722e96d798d5ac6f86
spiercer <snowpiercer2001@gmail.com>
2025-07-28 12:29:36 +0300

    removed calender resource for simplicity

--- src/calenders/calenders.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CalendersService } from './calenders.service';
-import { CalendersController } from './calenders.controller';
-
-@Module({
-  controllers: [CalendersController],
-  providers: [CalendersService],
-})
-export class CalendersModule {}
--- src/calenders/dto/update-calender.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCalenderDto } from './create-calender.dto';
-
-export class UpdateCalenderDto extends PartialType(CreateCalenderDto) {}
