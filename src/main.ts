9659609514c4c4f922f4c05146dca4e02862df2a
spiercer <snowpiercer2001@gmail.com>
2025-07-28 12:37:17 +0300

    configured scalar for API docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  try {
+    const config = new DocumentBuilder()
+      .setTitle('API')
+      .setDescription('API documentation')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+
+    app.use(
+      '/reference',
+      apiReference({
+        content: document,
+      }),
+    );
+    Logger.debug(
+      `Scalar is available at http://localhost:${port}/reference`,
+      'Scalar',
+    );
+  } catch (error) {
+    Logger.error('Failed to set up Scalar documentation:', error);
+  }
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  await app.listen(port);
 }
-bootstrap();
+
+void bootstrap();
